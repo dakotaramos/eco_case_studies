1b8f59f82c70dc829b716609542fefda17bac5ae
dakotaramos <ramos.dakotasky@gmail.com>
2022-05-09 15:38:13 -0600

    adding new plot_comparisons.py to with_battery_dispatch_and_grid parameter sweeps to obtain detailed finalized visuals

---
+++ parameter_sweep_results/flatirons_site/nrel_data/with_battery_dispatch_and_grid/plot_comparisons.py
@@ -0,0 +1,151 @@
+import matplotlib.pyplot as plt
+import openmdao.api as om
+import numpy as np
+from pathlib import Path
+
+root_dir = Path(__file__).parent.absolute()
+battery_capacity_file = root_dir / "DOE_battery_capacity_mwh_vs_all_objs" / "cases.sql"
+battery_power_file = root_dir / "DOE_battery_power_mw_vs_all_objs" / "cases.sql"
+interconnection_size_file = root_dir / "DOE_interconnection_size_mw_vs_all_objs" / "cases.sql"
+solar_size_file = root_dir / "DOE_solar_size_mw_vs_all_objs" / "cases.sql"
+wind_size_file = root_dir / "DOE_wind_size_mw_vs_all_objs" / "cases.sql"
+
+battery_cap_cr = om.CaseReader(battery_capacity_file)
+battery_power_cr = om.CaseReader(battery_power_file)
+interconnection_size_cr = om.CaseReader(interconnection_size_file)
+solar_size_cr = om.CaseReader(solar_size_file)
+wind_size_cr = om.CaseReader(wind_size_file)
+
+battery_cap_cases = battery_cap_cr.list_cases('driver')
+battery_power_cases = battery_power_cr.list_cases('driver')
+interconnection_size_cases = interconnection_size_cr.list_cases('driver')
+solar_size_cases = solar_size_cr.list_cases('driver')
+wind_size_cases = wind_size_cr.list_cases('driver')
+
+battery_cap_results = {}
+for case in battery_cap_cases:
+    outputs = battery_cap_cr.get_case(case).outputs
+    for key in outputs:
+
+        if key not in battery_cap_results.keys():
+            battery_cap_results[key] = []
+
+        battery_cap_results[key].append(outputs[key])
+
+for key in battery_cap_results:
+    battery_cap_results[key] = np.array(battery_cap_results[key]) 
+
+battery_power_results = {}
+for case in battery_power_cases:
+    outputs = battery_power_cr.get_case(case).outputs
+    for key in outputs:
+
+        if key not in battery_power_results.keys():
+            battery_power_results[key] = []
+
+        battery_power_results[key].append(outputs[key])
+
+for key in battery_power_results:
+    battery_power_results[key] = np.array(battery_power_results[key]) 
+
+interconnection_size_results = {}
+for case in interconnection_size_cases:
+    outputs = interconnection_size_cr.get_case(case).outputs
+    for key in outputs:
+
+        if key not in interconnection_size_results.keys():
+            interconnection_size_results[key] = []
+
+        interconnection_size_results[key].append(outputs[key])
+
+for key in interconnection_size_results:
+    interconnection_size_results[key] = np.array(interconnection_size_results[key]) 
+
+solar_size_results = {}
+for case in solar_size_cases:
+    outputs = solar_size_cr.get_case(case).outputs
+    for key in outputs:
+
+        if key not in solar_size_results.keys():
+            solar_size_results[key] = []
+
+        solar_size_results[key].append(outputs[key])
+
+for key in solar_size_results:
+    solar_size_results[key] = np.array(solar_size_results[key])
+
+wind_size_results = {}
+for case in wind_size_cases:
+    outputs = wind_size_cr.get_case(case).outputs
+    for key in outputs:
+
+        if key not in wind_size_results.keys():
+            wind_size_results[key] = []
+
+        wind_size_results[key].append(outputs[key])
+
+for key in wind_size_results:
+    wind_size_results[key] = np.array(wind_size_results[key])
+
+
+### scatter plots to visualize DOE results
+# Battery capacity MWh vs Hybrid LCOE
+X = battery_cap_results['battery_capacity_mwh']
+Y = battery_cap_results['hybrid_lcoe_real']
+
+plt.scatter(X, Y)
+plt.xlabel('Battery Capacity (MWh)')
+plt.ylabel('Hybrid system LCOE ($/kWh)')
+plt.title('Battery capacity vs Hybrid system LCOE')
+fname = 'battery_capacity_vs_hybrid_system_lcoe.pdf'
+plt.savefig(fname)
+plt.clf()
+
+# Battery power MW vs Hybrid LCOE
+X = battery_power_results['battery_power_mw']
+Y = battery_power_results['hybrid_lcoe_real']
+
+plt.scatter(X, Y)
+plt.xlabel('Battery Power (MW)')
+plt.ylabel('Hybrid system LCOE ($/kWh)')
+plt.title('Battery power vs Hybrid system LCOE')
+fname = 'battery_power_vs_hybrid_system_lcoe.pdf'
+plt.savefig(fname)
+plt.clf()
+
+# Interconnection size MW vs Hybrid LCOE
+X = interconnection_size_results['interconnection_size_mw']
+Y = interconnection_size_results['hybrid_lcoe_real']
+
+plt.scatter(X, Y)
+plt.xlabel('Grid interconnection size (MW)')
+plt.ylabel('Hybrid system LCOE ($/kWh)')
+plt.title('Interconnection size vs Hybrid system LCOE')
+fname = 'interconnection_size_vs_hybrid_system_lcoe.pdf'
+plt.savefig(fname)
+plt.clf()
+
+# Solar size MW vs Hybrid LCOE
+X = solar_size_results['solar_size_mw']
+Y = solar_size_results['hybrid_lcoe_real']
+
+plt.scatter(X, Y)
+plt.xlabel('Solar size (MW)')
+plt.ylabel('Hybrid system LCOE ($/kWh)')
+plt.title('Solar size vs Hybrid system LCOE')
+fname = 'solar_size_vs_hybrid_system_lcoe.pdf'
+plt.savefig(fname)
+plt.clf()
+
+# Wind size MW vs Hybrid LCOE
+X = wind_size_results['wind_size_mw']
+Y = wind_size_results['hybrid_lcoe_real']
+
+plt.scatter(X, Y)
+plt.xlabel('Wind size (MW)')
+plt.ylabel('Hybrid system LCOE ($/kWh)')
+plt.title('Wind size vs Hybrid system LCOE')
+fname = 'wind_size_vs_hybrid_system_lcoe.pdf'
+plt.savefig(fname)
+plt.clf()
+
